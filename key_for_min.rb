d9d54d7c056b54b0a7bdfddb882c731e14583a35
Lauren Stone <lstone321@gmail.com>
2020-07-19 03:55:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,9 @@
 def key_for_min_value(name_hash)
  
  lowest_number = 1000
- lowest_key = nil
- hash = {:blake => 500, :ashley => 2, :adam => 1}
+ lowest_key = nil 
  
-  hash.each do |key, value|
+  name_hash.each do |key, value|
    if  value < lowest_number
     lowest_number = value 
     lowest_key = key 
