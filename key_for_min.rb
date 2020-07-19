57f1771b67b23889e6d04471d913fa5fac6f0d67
Lauren Stone <lstone321@gmail.com>
2020-07-19 03:03:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
  hash = {:blake => 500, :ashley => 2, :adam => 1}
-  hash.collect do |number|
-  return number 
+  hash.collect do |key, value|
+  return value < 2 
 end
 end
